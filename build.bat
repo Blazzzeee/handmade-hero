05e6e106815fe16f6bf640fcf9d5953df2fbc83f
Blazzzeee <legendgamer1007@gmail.com>
2026-02-23 21:51:27 +0530

    fix windows batch script

--- build.bat
+++ build.bat
@@ -1,11 +1,20 @@
 @echo off
+setlocal
+
+REM Load Visual Studio Developer Environment
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
+
+if errorlevel 1 (
+    echo Failed to initialize MSVC environment.
+    exit /b 1
+)
 
 if not exist build mkdir build
 pushd build
 
-cl -Zi -nologo -W4 -WX -wd4201 -wd4100 -wd4189 ^
-   -Od -MTd ^
+cl -Zi -nologo -W4 -Od ^
    ..\main.cpp ^
    user32.lib gdi32.lib
 
 popd
+echo Build complete.
