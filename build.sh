99a7da5943f8a655db7d93b1d3999fc3b675e6e6
Blazzzeee <legendgamer1007@gmail.com>
2026-02-25 23:11:17 +0530

    modify wine build

--- build.sh
+++ build.sh
@@ -2,18 +2,36 @@
 
 echo "Starting build phase"
 set -euo pipefail
+
 SRC="main.cpp"
 DEBUG=${DEBUG:-0}
-BIN=target/obj.exe
+OUT_DIR="target"
+BIN="$OUT_DIR/handmade.exe"
 
-x86_64-w64-mingw32-gcc -O2 -Wall -Wextra -g -o "$BIN" $SRC $()
+mkdir -p "$OUT_DIR"
 
-echo "Compilation Successfull"
+if [ "$DEBUG" -eq 1 ]; then
+    echo "Building DEBUG"
+    x86_64-w64-mingw32-g++ \
+        -O0 -g -Wall -Wextra \
+        "$SRC" \
+        -o "$BIN" \
+        -lgdi32 -luser32
+else
+    echo "Building RELEASE"
+    x86_64-w64-mingw32-g++ \
+        -O2 -Wall -Wextra \
+        "$SRC" \
+        -o "$BIN" \
+        -lgdi32 -luser32
+fi
+
+echo "Compilation Successful"
 
 if [ "$DEBUG" -eq 0 ]; then
-    echo "Starting server without debugger"
-    wine ./"$BIN"
+    echo "Running with Wine"
+    wine "$BIN"
 else
-    echo "Starting server inside gdb"
-    gdb --args ./"$BIN"
+    echo "Running with winedbg (Wine debugger)"
+    winedbg "$BIN"
 fi
