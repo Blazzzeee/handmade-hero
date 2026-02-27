123959b3c47d3c646e8b2b053e8facc5bb2c72f1
Blazzzeee <legendgamer1007@gmail.com>
2026-02-27 21:52:46 +0530

    add winedbg bridge with gdb

--- build.sh
+++ build.sh
@@ -10,20 +10,28 @@ BIN="$OUT_DIR/handmade.exe"
 
 mkdir -p "$OUT_DIR"
 
+build() {
+    local mode="$1"
+
+    local CXX="x86_64-w64-mingw32-g++"
+    local CXXFLAGS="-Wall -Wextra"
+    local LDFLAGS="-lgdi32 -luser32"
+
+    if [ "$mode" = "debug" ]; then
+        echo "Building DEBUG"
+        CXXFLAGS="$CXXFLAGS -O0 -g"
+    else
+        echo "Building RELEASE"
+        CXXFLAGS="$CXXFLAGS -O2"
+    fi
+
+    $CXX $CXXFLAGS "$SRC" -o "$BIN" $LDFLAGS
+}
+
 if [ "$DEBUG" -eq 1 ]; then
-    echo "Building DEBUG"
-    x86_64-w64-mingw32-g++ \
-        -O0 -g -Wall -Wextra \
-        "$SRC" \
-        -o "$BIN" \
-        -lgdi32 -luser32
+    build debug
 else
-    echo "Building RELEASE"
-    x86_64-w64-mingw32-g++ \
-        -O2 -Wall -Wextra \
-        "$SRC" \
-        -o "$BIN" \
-        -lgdi32 -luser32
+    build release
 fi
 
 echo "Compilation Successful"
@@ -32,6 +40,6 @@ if [ "$DEBUG" -eq 0 ]; then
     echo "Running with Wine"
     wine "$BIN"
 else
-    echo "Running with winedbg (Wine debugger)"
-    winedbg "$BIN"
-fi
+    echo "Running with gdb + Wine"
+    winedbg --gdb "$BIN"
+ fi
