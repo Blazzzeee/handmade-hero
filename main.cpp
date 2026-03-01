7217bf7f3b41fcaf241ee4bf2630e80707260fe6
Blazzzeee <legendgamer1007@gmail.com>
2026-03-01 21:00:19 +0530

    added backbuffer for CPU rendering

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <debugapi.h>
+#include <windef.h>
 #include <windows.h>
 #include <wingdi.h>
 #include <winuser.h>
@@ -5,6 +7,45 @@
 #define persist static
 #define global static
 #define internal static
+
+#define GetCoordinates(rectangle)                                    \
+    x = (rectangle).left;                                            \
+    y = (rectangle).top;                                             \
+    width = (rectangle).right - (rectangle).left;                    \
+    height = (rectangle).bottom - (rectangle).top;
+
+global bool RUNNING;
+global BITMAPINFO BitmapInfo;
+global void *BitmapMemory;
+global HBITMAP BitmapHandle;
+global HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int width, int height) {
+
+    if (BitmapMemory) {
+        DeleteObject(BitmapMemory);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = width;
+    BitmapInfo.bmiHeader.biHeight = height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BitmapInfo.bmiHeader.biSizeImage = NULL;
+
+    BitmapHandle =
+        CreateDIBSection(BitmapDeviceContext, &BitmapInfo,
+                         DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int x, int y,
+                                int width, int height) {
+
+    StretchDIBits(DeviceContext, x, y, width, height, x, y, width,
+                  height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS,
+                  SRCCOPY);
+}
 
 internal LRESULT MainWindowProcedure(HWND WindowHandle, UINT msg,
                                      WPARAM WParam, LPARAM LParam) {
@@ -15,36 +56,43 @@ internal LRESULT MainWindowProcedure(HWND WindowHandle, UINT msg,
         res = 0;
     } break;
     case (WM_CLOSE): {
-        DestroyWindow(WindowHandle);
+        RUNNING = false;
+        // DestroyWindow(WindowHandle);
+        OutputDebugStringA("WM_CLOSE");
+    } break;
+    case (WM_DESTROY): {
+        RUNNING = false;
+        // PostQuitMessage(0);
     } break;
     case (WM_MOVE): {
     } break;
     case (WM_SIZE): {
+        int x, y, height, width;
+        OutputDebugStringA("WM_SIZE");
+        RECT client_rectangle;
+        GetClientRect(WindowHandle, &client_rectangle);
+
+        // convert rectangle into coordinates
+        GetCoordinates(client_rectangle);
+        Win32ResizeDIBSection(width, height);
     } break;
     case (WM_PAINT): {
         PAINTSTRUCT paint_info;
         HDC DeviceContext = BeginPaint(WindowHandle, &paint_info);
         if (!DeviceContext) {
-            // ig we should call the def process , assuming we cant
-            // process this req by the WM
+            // ig we should call the def process , assuming we
+            // cant process this req by the WM
             return DefWindowProcA(WindowHandle, msg, WParam, LParam);
         }
 
         int x, y, width, height;
+        RECT rect = paint_info.rcPaint;
 
-        x = paint_info.rcPaint.left;
-        y = paint_info.rcPaint.top;
+        GetCoordinates(rect);
 
-        width = paint_info.rcPaint.right - paint_info.rcPaint.left;
-        height = paint_info.rcPaint.bottom - paint_info.rcPaint.top;
-        if (!PatBlt(DeviceContext, x, y, width, height, BLACKNESS)) {
-            return DefWindowProcA(WindowHandle, msg, WParam, LParam);
-        }
+        Win32UpdateWindow(DeviceContext, x, y, width, height);
 
         EndPaint(WindowHandle, &paint_info);
-    } break;
-    case (WM_DESTROY): {
-        DestroyWindow(WindowHandle);
     } break;
     default: {
         return DefWindowProcA(WindowHandle, msg, WParam, LParam);
@@ -90,14 +138,12 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     // TODO: handle message queue
     MSG msg;
-    while ((ret = GetMessage(&msg, window_handle, 0, 0)) > 0) {
-        if (ret >= 0) {
+    RUNNING = true;
+    while (RUNNING) {
+        if (GetMessage(&msg, window_handle, 0, 0) > 0) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
-            if (ret == 0) {
-                // break here
-            }
-        } else if (ret == -1) {
+        } else {
             // Unknown error
             PostQuitMessage(0);
             break;
